bcbdc4ef9916ce49b7047e65c4e746a7af2e1c94
fatemah abdelaziz <fabdelaziz@smail.ucas.edu.ps>
2017-04-24 20:06:21 +0300

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,27 +1,27 @@
 public class BankAccount {
-
+//declare a variable its type private to see it inside this class not outside it
     private double balance;
-
-    public void BankAccount() {
+//a constractore without inial balance, name of the constractor must be the same of your class
+    public BankAccount() {
         balance = 0;
     }
-
-    public void BankAccount(double initialBalance) {
+//a costractore with inial balance 
+    public BankAccount(double initialBalance) {
         balance = initialBalance;
     }
-
+//a method called deposit (type: void it means return nothing) which increase the value of the balance
     public void deposit(double amount) {
         balance = balance + amount;
     }
-
+//a method called withdraw (type: void it means return nothing) which decrease the value of the balance
     public void withdraw(double amount) {
         balance = balance - amount;
     }
-
+//a method of the type double returns the balance 
     public double getBalance() {
         return balance;
     }
-
+//a method called addInsert (type: void it means return nothing) which add interest on the balance
     public void addInsert(double x) {
         balance = balance + balance * x / 100;
     }
